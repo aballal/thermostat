b9d93361d41419fac614fe699deb1f78a5f24390
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-08-02 15:27:20 +0100

    Added temperature up and down functions

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -6,6 +6,16 @@ describe('Thermostat', function () {
   });
 
   it('starts at 20 degrees', function () {
-    expect(thermostat.temperature).toBe(20);
+    expect(thermostat.getTemperature()).toEqual(20);
+  });
+
+  it('the temperature can be increased with up', function() {
+    thermostat.up();
+    expect(thermostat.getTemperature()).toEqual(21);
+  });
+
+  it('the temperature can be decreased with down', function() {
+    thermostat.down();
+    expect(thermostat.getTemperature()).toEqual(19);
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,14 @@ var Thermostat = function () {
   this.temperature = 20;
 };
 
-Thermostat.prototype.temperature = function () {
+Thermostat.prototype.getTemperature = function () {
   return this.temperature;
+};
+
+Thermostat.prototype.up = function () {
+  this.temperature += 1;
+};
+
+Thermostat.prototype.down = function () {
+  this.temperature -= 1;
 };
